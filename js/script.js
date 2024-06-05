37c8427f959a270ba071483511dd8afd9e505c33
cristian <chiat.contact@gmail.com>
2024-06-05 00:23:47 -0400

    mañana continuamos

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ class Paleta {
         this._colores = colores;
     }
 
-    // Getters
     get getNombre() {
         return this._nombre;
     }
@@ -13,7 +12,6 @@ class Paleta {
         return this._colores;
     }
 
-    // Setters
     set setNombre(nombre) {
         this._nombre = nombre;
     }
@@ -23,44 +21,6 @@ class Paleta {
     }
 }
 
-let paletaInicial = new Paleta("Paleta Inicial", ["#000000", "#FFFFFF", "#FF0000", "#00FF00", "#0000FF"]);
-let paleta2 = new Paleta("Paleta 2", ["#FFB3BA", "#FFDFBA", "#FFFFBA", "#BAFFC9", "#BAE1FF"]);
-let paleta3 = new Paleta("Paleta 3", ["#8B4513", "#A0522D", "#228B22", "#006400", "#8FBC8F"]);
-let paleta4 = new Paleta("Paleta 4", ["#000080", "#0000CD", "#1E90FF", "#87CEEB", "#E0FFFF"]);
-let paleta5 = new Paleta("Paleta 5", ["#7C0A02", "#D32F2F", "#FF5722", "#FF9800", "#FFC107"]);
-let paleta6 = new Paleta("Paleta 6", ["#FF4500", "#FF8C00", "#FFA500", "#FFD700", "#FFFF00"]);
-let paleta7 = new Paleta("Paleta 7", ["#2E2B5F", "#4B0082", "#8A2BE2", "#DA70D6", "#EE82EE"]);
-
-let paletasPredefinidas = [paletaInicial, paleta2, paleta3, paleta4, paleta5, paleta6, paleta7];
-
-document.addEventListener("DOMContentLoaded", function() {
-    cargarSelectPaletasIniciales();
-});
-
-function cargarSelectPaletasIniciales() {
-    let select = document.getElementById('select-paletas');
-    paletasPredefinidas.forEach((paleta, index) => {
-        let option = document.createElement('option');
-        option.value = index;
-        option.innerText = paleta.getNombre;
-        select.appendChild(option);
-    });
-
-    // Mostrar la primera paleta por defecto
-    mostrarPaletaSeleccionada();
-}
-
-function mostrarPaletaSeleccionada() {
-    let select = document.getElementById('select-paletas');
-    let index = select.value;
-    let paleta = paletasPredefinidas[index];
-    let paletaContainer = document.getElementById("paletaSeleccionadaContainer");
-    paletaContainer.innerHTML = `<h3>${paleta.getNombre}</h3>`;
-    paleta.getColores.forEach(color => {
-        paletaContainer.innerHTML += `<div class="color-picker" style="background-color:${color}; width:20px; height:20px; display:inline-block; margin:2px;"></div>`;
-    });
-}
-
 class Escena {
     constructor(nombre, pixeles, palette = paletaInicial) {
         this._nombre = nombre;
@@ -68,7 +28,6 @@ class Escena {
         this._palette = palette;
     }
 
-    // Getters
     get getNombre() {
         return this._nombre;
     }
@@ -81,7 +40,6 @@ class Escena {
         return this._palette;
     }
 
-    // Setters
     set setNombre(nombre) {
         this._nombre = nombre;
     }
@@ -104,20 +62,19 @@ class Proyecto {
         this._escena = escena;
     }
 
-    // Getters
     get getId() {
         return this._id;
     }
 
-    get nombre() {
+    get getNombre() {
         return this._nombre;
     }
 
-    get tamaño() {
+    get getTamaño() {
         return this._tamaño;
     }
 
-    get fechaCreacion() {
+    get getFechaCreacion() {
         return this._fechaCreacion;
     }
 
@@ -125,7 +82,6 @@ class Proyecto {
         return this._escena;
     }
 
-    // Setters
     set nombre(nombre) {
         this._nombre = nombre;
     }
@@ -138,8 +94,60 @@ class Proyecto {
         this._fechaCreacion = fechaCreacion;
     }
 }
+let findProyecto = function() {
+    let buscar = document.getElementById("b-nom").value;
+    let p = proyectos.find(item => item.getNombre === buscar);
 
+    if (p != undefined) {
+        let rp = document.createElement("span");
+        rp.innerText = "Id: " + p.getId + " Nombre: " + p.getNombre + " Tamaño: "+ p.getTamaño +" Fecha Creacion: " + p.getFechaCreacion + " Escena: "+p.getEscena.getNombre + " Resolucion: " + p.getEscena.getPixeles+" x "+ p.getEscena.getPixeles+ " Pixeles"+ " Paleta:"+p.getEscena.getPalette.getNombre;
+        document.getElementById("Resultado").innerHTML = "";
+        document.getElementById("Resultado").appendChild(rp);
+    } else {
+        alert("No Encontrada");
+        document.getElementById("Resultado").innerHTML = "";
+        document.getElementById("upd-data").innerHTML = ""; 
+    }
+}
+let paletaInicial = new Paleta("Paleta Inicial", ["#000000", "#FFFFFF", "#FF0000", "#00FF00", "#0000FF"]);
+let paleta2 = new Paleta("Paleta 2", ["#FFB3BA", "#FFDFBA", "#FFFFBA", "#BAFFC9", "#BAE1FF"]);
+let paleta3 = new Paleta("Paleta 3", ["#8B4513", "#A0522D", "#228B22", "#006400", "#8FBC8F"]);
+let paleta4 = new Paleta("Paleta 4", ["#000080", "#0000CD", "#1E90FF", "#87CEEB", "#E0FFFF"]);
+let paleta5 = new Paleta("Paleta 5", ["#7C0A02", "#D32F2F", "#FF5722", "#FF9800", "#FFC107"]);
+let paleta6 = new Paleta("Paleta 6", ["#FF4500", "#FF8C00", "#FFA500", "#FFD700", "#FFFF00"]);
+let paleta7 = new Paleta("Paleta 7", ["#2E2B5F", "#4B0082", "#8A2BE2", "#DA70D6", "#EE82EE"]);
+
+let paletasPredefinidas = [paletaInicial, paleta2, paleta3, paleta4, paleta5, paleta6, paleta7];
 let proyectos = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+    cargarSelectPaletasIniciales();
+    if (window.location.pathname.endsWith("Escena.html")) {
+        loadEscena();
+    }
+});
+
+function cargarSelectPaletasIniciales() {
+    let select = document.getElementById('select-paletas');
+    paletasPredefinidas.forEach((paleta, index) => {
+        let option = document.createElement('option');
+        option.value = index;
+        option.innerText = paleta.getNombre;
+        select.appendChild(option);
+    });
+    mostrarPaletaSeleccionada();
+}
+
+function mostrarPaletaSeleccionada() {
+    let select = document.getElementById('select-paletas');
+    let index = select.value;
+    let paletaIndex = document.getElementById("select-paletas").value;
+    let paletaInicial = paletasPredefinidas[paletaIndex];
+    let paletaContainer = document.getElementById("paletaSeleccionadaContainer");
+    paletaInicial.getColores.forEach(color => {
+        paletaContainer.innerHTML += `<div class="color-picker" style="background-color:${color};"></div>`;
+    });
+}
 
 function crearProyecto() {
     let nom = document.getElementById("p-nom").value;
@@ -162,23 +170,58 @@ function crearProyecto() {
 function updateTable() {
     let tableBody = document.getElementById("table-body");
     tableBody.innerHTML = "";
-    
+
     proyectos.forEach((proyecto, index) => {
-        let paletaColores = proyecto.getEscena.getPalette.getColores.map(color => `<div class="color-picker" style="background-color:${color}; width:20px; height:20px; display:inline-block; margin:2px;"></div>`).join('');
-        let row = `<tr>
-            <td>${proyecto.getId}</td>
-            <td><a href="site/Escena.html" onclick="saveEscena(${index})">${proyecto.nombre}</a></td>
-            <td>${proyecto.tamaño}</td>
-            <td>${proyecto.fechaCreacion}</td>
-            <td>${proyecto.getEscena}</td>
-            <td>${paletaColores}</td>
-        </tr>`;
-        tableBody.innerHTML += row;
+        let paletaColores = proyecto.getEscena.getPalette.getColores.map(color => `<div class="color-picker" style="background-color:${color};"></div>`).join('');
+        tableBody.innerHTML += `
+            <tr>
+                <td>${proyecto.getId}</td>
+                <td><a href="site/Escena.html" onclick="saveEscena(${index})">${proyecto.getNombre}</a></td>
+                <td>${proyecto.getTamaño}</td>
+                <td>${proyecto.getFechaCreacion}</td>
+                <td> Escena: ${proyecto.getEscena.getNombre} Resolucion: ${proyecto.getEscena.getPixeles} x ${proyecto.getEscena.getPixeles} Pixeles Paleta de colores: ${proyecto.getEscena.getPalette.getNombre}</td>
+                <td class="d-flex flex-wrap">${paletaColores}</td>
+            </tr>
+        `;
     });
 }
 
 function saveEscena(index) {
     localStorage.setItem("currentEscena", JSON.stringify(proyectos[index].getEscena));
+}
+
+let escenaActual = null;
+
+function loadEscena() {
+    let escenaData = localStorage.getItem("currentEscena");
+    if (escenaData) {
+        let escenaObj = JSON.parse(escenaData);
+        escenaActual = new Escena(escenaObj._nombre, escenaObj._pixeles, new Paleta(escenaObj._palette._nombre, escenaObj._palette._colores));
+        document.getElementById("PROYECTO-NAME").textContent = escenaActual.getNombre;
+        crearCuadricula(escenaActual);
+        mostrarPaleta(escenaActual.getPalette);
+        mostrarTodasPaletas();
+        cargarSelectPaletas();
+    } else {
+        alert("No se pudo cargar la escena.");
+    }
+}
+
+function crearCuadricula(escena) {
+    let container = document.getElementById("CUADRICULA");
+    container.innerHTML = "";
+    for (let i = 0; i < escena.getPixeles; i++) {
+        let row = document.createElement("div");
+        row.className = "grid-row";
+        for (let j = 0; j < escena.getPixeles; j++) {
+            let cell = document.createElement("div");
+            cell.className = "grid-cell";
+            cell.id = `cell-${i}-${j}`;
+            cell.onclick = () => changeColor(i, j);
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
 }
 
 function mostrarPaleta(paleta) {
@@ -191,23 +234,7 @@ function mostrarPaleta(paleta) {
 
 function seleccionarColor(color) {
     colorSeleccionado = color;
-    console.log(`Color seleccionado: ${colorSeleccionado}`);
-}
-
-let escenaActual = null;
-
-function loadEscena() {
-    let escenaData = localStorage.getItem("currentEscena");
-    if (escenaData) {
-        let escenaObj = JSON.parse(escenaData);
-        escenaActual = new Escena(escenaObj._nombre, escenaObj._pixeles, new Paleta(escenaObj._palette._nombre, escenaObj._palette._colores));
-        crearCuadricula(escenaActual);
-        mostrarPaleta(escenaActual.getPalette);
-        mostrarTodasPaletas();
-        cargarSelectPaletas();
-    } else {
-        alert("No se pudo cargar la escena.");
-    }
+    console.log(`Color seleccionado: ${color}`);
 }
 
 function cargarSelectPaletas() {
@@ -324,3 +351,15 @@ function changeColor(i, j) {
 
 window.crearProyecto = crearProyecto;
 window.crearCuadricula = crearCuadricula;
+window.saveEscena = saveEscena;
+window.loadEscena = loadEscena;
+window.mostrarPaleta = mostrarPaleta;
+window.cargarSelectPaletas = cargarSelectPaletas;
+window.mostrarTodasPaletas = mostrarTodasPaletas;
+window.asignarPaleta = asignarPaleta;
+window.actualizarSelectPaletas = actualizarSelectPaletas;
+window.crearNuevaPaleta = crearNuevaPaleta;
+window.editarPaleta = editarPaleta;
+window.guardarCambiosPaleta = guardarCambiosPaleta;
+window.changeColor = changeColor;
+window.seleccionarColor = seleccionarColor;
